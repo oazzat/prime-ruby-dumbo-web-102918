5b869953fce80512581e67effc28b4351d514e07
Omar A <oazzat19@gmail.com>
2018-09-29 17:12:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime? (num)
+
+  array = *(1..num)
+  
+  if num < 2 
+    return false
+  end
+  
+  array.each do |int|
+    if num % int == 0
+      if int > 1
+        if num !=2 && num != int
+          return false
+        end
+      end
+    end
+        
+  end
+  
+  
+return true
+end
